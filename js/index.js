03f6d02f714f9c8d742e2a1315035ef19ba1c724
UsanaseD <todiddy20@gmail.com>
2020-10-06 01:11:41 +0200

    fweerw

--- js/index.js
+++ js/index.js
@@ -3,15 +3,15 @@ const login = document.getElementById("login");
 
 login.addEventListener("click", (e) => {
   if (user.value === "gitifu") {
-    window.location.href = "./dashboard.html";
+    window.location.href = "../dashboard.html";
   } else if (user.value === "education_sector") {
-    window.location.href = "./education.html";
+    window.location.href = "../education.html";
   } else if (user.value === "education_district") {
-    window.location.href = "./educ_dist.html";
+    window.location.href = "../educ_dist.html";
   } else if (user.value === "mudugudu") {
-    window.location.href = "./mudugudu.html";
+    window.location.href = "../mudugudu.html";
   } else if (user.value === "imibereho") {
-    window.location.href = "./imibereho.html";
+    window.location.href = "../imibereho.html";
   } else {
     alert("Account doesn't exist!");
   }
