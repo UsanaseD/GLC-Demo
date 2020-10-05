b0746fdf19088f04acaa4ef0a11fa70357fc218b
UsanaseD <todiddy20@gmail.com>
2020-10-05 23:38:52 +0200

    first commit

---
+++ js/dist.js
@@ -0,0 +1,15 @@
+const dash = document.getElementById('dist');
+const header = document.getElementById('header')
+const one = document.getElementById('one');
+const t = document.getElementById('2');
+const th = document.getElementById('3');
+const fo = document.getElementById('4');
+const fi = document.getElementById('5');
+const si = document.getElementById('6');
+
+window.addEventListener('load',(e)=>{
+    dash.style.display='none';
+})
+one.addEventListener('click',(e)=>{
+    header.style.display='none';
+})
